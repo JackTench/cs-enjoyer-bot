53441ae3c11007210986969f522be2653a61837d
JackTench <79285604+JackTench@users.noreply.github.com>
2026-03-12 18:32:29 +0000

    Create event loop for Python 3.14 and newer.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import asyncio
 import os
 
 import discord
 from discord.ext import commands
+
+# Create and set an event loop.
+# Explicitly for Python 3.14+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
 
 # Set up bot.
 intents = discord.Intents.default()
